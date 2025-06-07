67a2a153bb99d7cc8f5991e0dd1e750e7ca3737a
quyen <quyen2909a@gmail.com>
2025-06-08 00:33:13 +0700

    aaa

--- Migrations/20250606173145_InitModels.Designer.cs
+++ Migrations/20250606173145_InitModels.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VAYTIEN.Models;
 
@@ -11,9 +12,11 @@ using VAYTIEN.Models;
 namespace VAYTIEN.Migrations
 {
     [DbContext(typeof(QlvayTienContext))]
-    partial class QlvayTienContextModelSnapshot : ModelSnapshot
+    [Migration("20250606173145_InitModels")]
+    partial class InitModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
