ef0614df202e697a53bf13b0db10d6a1277c90cf
NguyenSon0403 <son2004ntt@gmail.com>
2025-06-11 09:49:17 +0700

    l

---
+++ Models/ThongTinVayViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace VAYTIEN.Models
+
+{
+    public class ThongTinVayViewModel
+    {
+        public decimal TongSoTienVay { get; set; }
+        public List<ThongTinHopDongViewModel> HopDongs { get; set; }
+    }
+
+    public class ThongTinHopDongViewModel
+    {
+        public int MaHopDong { get; set; }
+        public decimal? SoTienVay { get; set; }
+        public DateOnly? NgayVay { get; set; }
+        public DateOnly? NgayHetHan { get; set; }
+        public List<LichTraViewModel> LichTra { get; set; }
+    }
+
+    public class LichTraViewModel
+    {
+        public int KyHanThu { get; set; }
+        public DateOnly? NgayTra { get; set; }
+        public decimal? SoTienPhaiTra { get; set; }
+        public string? TrangThai { get; set; }
+    }
+}
